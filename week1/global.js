df518ad40a06dbc411d84ec98b93e3546530ea8f
Ju0011 <yhs07240@naver.com>
2022-12-28 14:44:34 +0900

    first commit

---
+++ week1/global.js
@@ -0,0 +1,17 @@
+// window.onload = function () {
+//     var element = document.getElementById("hi");
+
+//     console.log("여기에 뭐 있니?>>>>>", element)
+
+//     function eventhandler() {
+//         alert("동작")
+//     }
+
+//     /* element.addEventListener("cllick", 
+//         function (){
+//         alert("Hello World!");
+//     }) */
+//     element.addEventListener("click", eventhandler);
+// };
+
+console.log("hello")
